bea08cc32ff4cf9d0a29fb0282486264558f04ca
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-03-24 01:21:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "1735152469"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "1735152469"))
 
 #Port
-PORT = os.environ.get("PORT", "1000")
+PORT = os.environ.get("PORT", "1100")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://animebash212:animebash212@cluster0.lhtmajy.mongodb.net/?retryWrites=true&w=majority")
