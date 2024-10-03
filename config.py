aae71f5fac7c77e1b506c86f9a81b259e807da4a
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-10-03 17:35:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002027498504"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "1735152469"))
 
 #Port
-PORT = os.environ.get("PORT", "7011")
+PORT = os.environ.get("PORT", "7012")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Sistaanimebash:Sistaanimebash@cluster0.w6ucx5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
