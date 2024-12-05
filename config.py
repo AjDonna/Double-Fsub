6b6c0b8fc76c77a901b4cd9c467955fa34346eba
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-12-05 23:11:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,30 +10,30 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7583394186:AAFsVX0HxkPP53rl3RNss7Xcgk5S3O5Gi5E")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7116038251:AAHuiNXwjbwwie8AxNxD97X15dwZ707q3q8")
 
-# Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "22409622"))
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "7603458"))
 
-# Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "16353e2a4d45ff8be4a2037cca158749")
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "910e420f1f74f40305a684a331dade35")
 
-# Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002318031286"))
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002027498504"))
 
-# OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "7211073530"))
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "1735152469"))
 
-# Port
-PORT = os.environ.get("PORT", "8080")
+#Port
+PORT = os.environ.get("PORT", "0987")
 
-# Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Toonpro12:animebash@cluster0.e6hpn8l.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "dschut")
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Sistaanimebash:Sistaanimebash@cluster0.w6ucx5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-# Force sub channel id, if you want enable force sub
-FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1001715616229"))
-FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1001974461884"))
+#force sub channel id, if you want enable force sub
+FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001715616229"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001974461884"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -41,7 +41,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "<b> Kᴏɴɴɪᴄʜɪᴡᴀ {first} 👋 </b>\n\nI ᴀᴍ ᴀ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ @Anime_Bash")
 try:
     ADMINS=[6376328008]
-    for x in (os.environ.get("ADMINS", "1119579816 6427494689 6590736993 6315792232 1735152469 5574593875 7211073530").split()):
+    for x in (os.environ.get("ADMINS", "7211073530 1735152469 1119579816 1632095203 6315792232 5350104697").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -59,7 +59,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "​🚫Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ​ - @Anime_Bash"
+USER_REPLY_TEXT = "🚫Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - @Anime_Bash"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(5090651635)
@@ -84,3 +84,4 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+    
